077f79c90df2aa96a74a1d47304f160e7c6a9b0f
hlassiege <hugo@malt.com>
2022-08-25 10:17:57 +0200

    Change output format to esm

--- navbar/vite.config.js
+++ navbar/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
-        format: 'system',
+        format: 'esm',
         minifyInternalExports: false
       }
     }
